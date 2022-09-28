e4a5538869d2f3210202cd77b0d9a4b265a9d450
bugashesh <valeria.bugak@icloud.com>
2022-09-28 23:02:49 +0300

    create Lab 3

---
+++ Lab__3/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab__3
+{
+    public class Person
+    {
+        public string name = "Undefined";
+        public int age;
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+
+    internal class Book
+    {
+        public int pages;
+        public string name;
+        public float weight;
+
+        public void getInfoBook()
+        {
+            Console.WriteLine("В книге " + name + " находиться " + pages + " страниц. ");
+            Console.WriteLine("При этом она весит " + weight);
+        }
+    }
+}
